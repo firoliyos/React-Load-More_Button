71010ebe01e7c874f5ead71aef5253730f15f1bb
firoliyos <firatesfaye44@gmail.com>
2024-10-19 15:10:45 +0300

    loading function

--- load-more-button/src/components/index.jsx
+++ load-more-button/src/components/index.jsx
@@ -9,9 +9,13 @@ export default function LoadMoreData() {
 
    async function fetchProducts() {
       try{
+        setLoading(true)
         const response = await fetch(`https://dummyjson.com/products?limit=100&skip=${count === 0 ? 0 : count *20}`)
         const result = await response.json()
-        setLoading(true)
+        if(result && result.products && result.products.length)  {
+           setProducts(result.products)
+           setLoading(false)
+         }
         console.log(result)
       }catch(e){
         setLoading(false)
@@ -22,11 +26,17 @@ export default function LoadMoreData() {
    useEffect(() => {
       fetchProducts()
     }, [])
+
+   if(loading){
+      return(
+         <div className="loading-container">Loading Data...</div>
+      )
+   } 
     
    
      return(
       <div className="load-more-container">
-         Nigga
+        
       </div>
      )
    }
