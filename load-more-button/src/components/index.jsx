c1c84b33066bab3dfc2073e27da10e78a076cc1f
firoliyos <firatesfaye44@gmail.com>
2024-10-19 14:54:14 +0300

    useStates written!

--- load-more-button/src/components/index.jsx
+++ load-more-button/src/components/index.jsx
@@ -3,63 +3,16 @@ import {useState, useEffect} from 'react'
 
 export default function LoadMoreData() {
 
-  const [loading, setLoading] = useState(false)
-  const [products, setProducts] = useState([])
-  const [count, setCount] = useState(false)
-  const [disableButton, setDisableButton] = useState(false)
-  async function fetchProducts() {
-    try {
-      setLoading(true)
-      const response = await fetch(`https://dummyjson.com/products?limit=20&skip${count === 0 ? 0 : count *20}`)
-      const result = await response.json()
-      if(result && result.products &&result.products.length) {
-         setProducts(prevData => [...prevData, ...result.products])
-         setLoading(false)
-         console.log(result)
-      }
-    }catch(e) {
-      setLoading(false)
-      console.log(e)
-    }
-  }
-   
-  useEffect(() => {
-   fetchProducts()
-  }, [count])
+   const [loading, setLoading] = useState(false)
+   const [products, setProducts] = useState([])
+   const [count, setCount] = useState(0)
+  
 
-  useEffect(() => {
-    if(products && products.length === 100) setDisableButton(true)
-  }, [products])
-
-  if(loading) {
-   return(
-      <div>
-        Loading... 
-      </div>
-   )
-  }
+    
    
      return(
       <div className="load-more-container">
-         <div className="product-container">
-            {
-             products && products.length ? 
-              products.map(item => <div className="product" key={item.title}>
-               <img src={item.thumbnail} alt={item.title} />
-               <p>{item.title}</p>
-              </div> )
-             : null  
-            }
-         </div>
-         <div className="button-container">
-           <button disabled = {disableButton }
-            onClick={()=> setCount(count + 1)}>
-             Load More Items
-           </button>
-           {
-            disableButton && <p>You have Reached Maximum Products!</p>
-           }
-         </div>
+         Nigga
       </div>
      )
-}
+   }
