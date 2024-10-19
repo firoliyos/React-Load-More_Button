d3f35c7fff54c24cefc26e96a1f73fe4b43ab051
firoliyos <firatesfaye44@gmail.com>
2024-10-19 15:28:18 +0300

    products displayed

--- load-more-button/src/components/index.jsx
+++ load-more-button/src/components/index.jsx
@@ -36,7 +36,16 @@ export default function LoadMoreData() {
    
      return(
       <div className="load-more-container">
-        
+        <div className="product-container">
+         {
+           products && products.length ? 
+            products.map(item => <div className="product" key={item.id}>
+              <img src={item.thumbnail} alt={item.title} />  
+              <p>{item.title}</p>
+            </div>)
+           : null 
+         }
+        </div>
       </div>
      )
    }
